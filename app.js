d37d04e310908433f2f490fc8a84e498be9d42a4
Mike Kunze <michael.kunze@gmail.com>
2011-09-11 21:33:26 -0500

    express added

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
-
-/**
- * Module dependencies.
- */
-
+var fs      = require('fs');
+var sys     = require('sys');
+var async   = require('async');
 var express = require('express');
+
+var path    = '/mnt/iScanLogs';
+
+var beadChip = require('./beadChip.js');
+
+var metricsFiles = [];
+var beadChips    = [];
+
+var populateMetrics = function() {
+  metricsFiles = [];
+
+  var addMetric = function(file, callback) {
+      fs.stat(path + '/' + file, function(err, stats) {
+        if(stats.isDirectory()) {
+          fs.stat(path + '/' + file + '/Metrics.txt', function(err, stats) {
+            if(err) {
+              callback();
+            } else {
+              metricsFiles.push(path + '/' + file + '/Metrics.txt');
+              callback();
+            }
+          })
+        } else callback();
+      });    
+  };
+
+  fs.readdir(path, function(err, files) {
+    if(err) console.log(err);
+
+    async.forEach(files, addMetric, function(err) {
+      if(err) console.log(err);
+      console.log('************************\n' + metricsFiles.length + ' metric files found');
+      scanMetrics();
+    });
+  });
+};
+
+var scanMetrics = function() {
+  beadChips = [];
+
+  var singleScan = function(file, callback) {
+    beadChips.push(new beadChip(file));
+    callback();
+  };
+
+  async.forEach(metricsFiles, singleScan, function(err) {
+    console.log(beadChips.length + ' beadchips initialized\n************************'); 
+  });
+};
+
+populateMetrics();
+
 
 var app = module.exports = express.createServer();
 
+var io  = require('socket.io').listen(app);
 // Configuration
 
 app.configure(function(){
@@ -14,6 +65,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use('/extjs', express.static(__dirname + '/extjs'));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -30,9 +82,9 @@ app.configure('production', function(){
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'iScan Metrics'
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(80);
+console.log("extstack server listening on port %d in %s mode", app.address().port, app.settings.env);
